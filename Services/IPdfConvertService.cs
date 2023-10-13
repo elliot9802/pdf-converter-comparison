32973383c357bc8b5fcb7e7f9061bef34f5f9854
ElliotSvtc <Elliot.Segerlind@vitecsoftware.com>
2023-10-13 16:06:56 +0200

    Add project files.

---
+++ Services/IPdfConvertService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IPdfConvertService
+    {
+        void ConvertHtmlToPdf(string htmlContent, string outputPath);
+        void ConvertUrlToPdf(string urlContent, string outputPath);
+    }
+}
---
+++ Services/NRecoConvertService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using NReco.PdfGenerator;
+
+namespace Services
+{
+    public class NRecoConvertService : IPdfConvertService
+    {
+        public void ConvertHtmlToPdf(string htmlContent, string outputPath)
+        {
+            var htmlToPdf = CreateHtmlToPdfConverter();
+
+            try
+            {
+                var pdfBytes = htmlToPdf.GeneratePdf(htmlContent);
+                File.WriteAllBytes(outputPath, pdfBytes);
+            }
+            catch (Exception)
+            {
+                // Throw the exception so it can be caught and logged/handled at the caller.
+                throw;
+            }
+        }
+
+        public void ConvertUrlToPdf(string urlContent, string outputPath)
+        {
+            var htmlToPdf = CreateHtmlToPdfConverter();
+
+            try
+            {
+                htmlToPdf.GeneratePdfFromFile(urlContent, null, outputPath);
+            }
+            catch (Exception)
+            {
+                // Throw the exception so it can be caught and logged/handled at the caller.
+                throw;
+            }
+        }
+
+        private HtmlToPdfConverter CreateHtmlToPdfConverter()
+        {
+            return new HtmlToPdfConverter();
+            // Add specific configurations to set for every instance of HtmlToPdfConverter
+            // For instance:
+            // converter.PageFooterHtml = null;
+            // return converter;
+        }
+    }
+}
